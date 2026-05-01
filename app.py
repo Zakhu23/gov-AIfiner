ebe407da03fce98499c4d49e4838a0e87ff7d05f
Zakhu23 <zakhwan5168@gmail.com>
2026-05-01 19:35:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return """
+    <h1>GovAI Working ✅</h1>
+    <form method="POST" action="/result">
+        Age: <input type="number" name="age"><br><br>
+        <button type="submit">Submit</button>
+    </form>
+    """
+
+@app.route("/result", methods=["POST"])
+def result():
+    return "<h2>Form Submitted Successfully 🚀</h2>"
