b83e11b399e80636ae34c112d220d52c326f6f57
Zakhu23 <zakhwan5168@gmail.com>
2026-05-01 19:15:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "WORKING SUCCESSFULLY 🚀"
+    results = []
+
+    if request.method == "POST":
+        results = [
+            ("PM Kisan", "Farmer scheme"),
+            ("SSC CGL", "Government exam"),
+            ("UP Scholarship", "Student scheme")
+        ]
+
+    return render_template("index.html", results=results)
 
 if __name__ == "__main__":
     app.run()
