c13abe106198efc740846f40eb7288d3acb24b24
Zakhu23 <zakhwan5168@gmail.com>
2026-05-03 00:01:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-from flask import Flask, render_template
-import os
+from flask import Flask
 
-# 🔥 FORCE TEMPLATE PATH (THIS FIXES YOUR ERROR)
-template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
-
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run()
+    return "FINAL TEST SUCCESS"
